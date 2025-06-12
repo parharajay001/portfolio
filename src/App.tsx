a957bee6a0ace253882f9aec8d3f998a89b41fa3
Ajay Parhar <parharajay001@gmail.com>
2025-06-12 13:56:44 +0530

    feat: Implement Navigation component and update App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import Portfolio from './pages/Portfolio/Portfolio';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className='bg-primary'>
-      <Portfolio />
+      <Navigation />
+      <Hero />
+      <Experience />
+      <Projects />
+      <Contact />
     </div>
   );
 }
